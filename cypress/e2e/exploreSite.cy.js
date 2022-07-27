7e0a3ebedefda67f40b6573668449172bec64f2c
Leszko <lkubikowski@gmail.com>
2022-07-27 17:03:22 +0200

    last changes

--- cypress/e2e/exploreSite.cy.js
+++ cypress/e2e/exploreSite.cy.js
@@ -1,14 +1,13 @@
 describe("empty spec", () => {
   beforeEach(() => {
-    // reset and seed the database prior to every test
     cy.visit("/")
   })
-  it("passes", () => {
+
+  it("should calculate regular sallary", () => {
     cy.visit("https://info.espeo.eu/matrix-calculator-list")
 
-    cy.get('[href="/matrix-calculator/qa"]').click()
     cy.get(
-      '[js-matrixcalc-set-preset="regular-3"] > .matrixcalc-presets__item-inner > .matrixcalc__preset-item-text'
+      ".is-current > .matrixcalc-presets__item-inner > .matrixcalc__preset-item-text"
     ).click()
     cy.get(
       ':nth-child(2) > .matrixcalc-summary__salary-amount-text > [js-matrix-result-regular-3="-"]'
@@ -17,5 +16,35 @@ describe("empty spec", () => {
     cy.get(
       '[js-matrixcalc-skill-block="tech-1"] > .matrixcalc-skill__body > .matrixcalc-skill__slider > .generic-slider > .generic-slider__drag-area > .generic-slider__steps-indicators > :nth-child(2)'
     ).click()
+    cy.get(
+      '[js-matrixcalc-skill-block="soft-4"] > .matrixcalc-skill__body > .matrixcalc-skill__slider > .generic-slider > .generic-slider__drag-area > .generic-slider__steps-indicators > :nth-child(3)'
+    ).click()
+  })
+
+  it("should junior salary calculate", () => {
+    cy.visit("https://info.espeo.eu/matrix-calculator-list")
+
+    cy.get('[href="/matrix-calculator/qa"]').click()
+    cy.get(
+      '[js-matrixcalc-set-preset="junior-1"] > .matrixcalc-presets__item-inner > .matrixcalc__preset-item-text'
+    ).click()
+    cy.get(
+      ':nth-child(2) > .matrixcalc-summary__salary-amount-text > [js-matrix-result-junior-1="-"]'
+    ).should("have.text", "4 440")
+
+    cy.get(
+      '[js-matrixcalc-skill-block="tech-1"] > .matrixcalc-skill__body > .matrixcalc-skill__slider > .generic-slider > .generic-slider__drag-area > .generic-slider__steps-indicators > :nth-child(2)'
+    ).click()
+    cy.get(
+      '[js-matrixcalc-skill-block="soft-4"] > .matrixcalc-skill__body > .matrixcalc-skill__slider > .generic-slider > .generic-slider__drag-area > .generic-slider__steps-indicators > :nth-child(3)'
+    ).click()
+  })
+  it.only("should contact with espeo", () => {
+    cy.visit("https://espeo.eu/contact/")
+
+    cy.get(".optin > .cmplz-buttons > .cmplz-accept").click()
+    cy.scrollTo(0, 50)
+    //cy.get(".hamburger-box").click()
+    //cy.get("#email-2daf1607-fb43-4e11-8731-0419a4fd9855").type("fasdf")
   })
 })
